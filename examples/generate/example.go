af6b92ba3c23f2dbb8569c3a2e600066e5a81380
crgimenes <crg@crg.eti.br>
2023-08-08 23:11:27 -0300

    update to go 1.21

--- examples/generate/example.go
+++ examples/generate/example.go
@@ -19,13 +19,13 @@ type GlossList struct {
 }
 
 type GlossEntry struct {
-	Abbrev    string `json:"Abbrev"`
-	Acronym   string `json:"Acronym"`
-	GlossDef  `json:"GlossDef"`
-	GlossSee  string `json:"GlossSee"`
-	GlossTerm string `json:"GlossTerm"`
-	ID        string `json:"ID"`
-	SortAs    string `json:"SortAs"`
+	Abbrev    string   `json:"Abbrev"`
+	Acronym   string   `json:"Acronym"`
+	GlossDef  GlossDef `json:"GlossDef"`
+	GlossSee  string   `json:"GlossSee"`
+	GlossTerm string   `json:"GlossTerm"`
+	ID        string   `json:"ID"`
+	SortAs    string   `json:"SortAs"`
 }
 
 type GlossDef struct {
